98233d8b0398e77fe67ebc2b918dd75aef10d687
arunponugoti1 <arun.devop1@gmail.com>
2026-04-30 16:22:06 +0000

    Implement end-to-end User Registration demo

--- infrastructure/storage.tf
+++ infrastructure/storage.tf
@@ -1,7 +1,10 @@
+
+
 resource "google_storage_bucket" "backup" {
   name          = "enterprise-backups-${var.project_id}"
   location      = var.region
   force_destroy = true
 
   uniform_bucket_level_access = true
+
 }
